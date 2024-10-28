5786dd252350408c87ecf14503541958b066c77b
eckepecke <erik_olofsson@icloud.com>
2024-10-28 21:19:32 +0900

    Adding all testfiles again.

--- jest-backend.config.js
+++ jest-backend.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: [
-        '<rootDir>/test/**/docs.js',
-        '<rootDir>/test/**/auth.js'
+        '<rootDir>/test/**/*.js'
     ],
     coverageDirectory: 'coverage/backend',
     collectCoverage: true,
